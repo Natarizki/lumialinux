97b84e9f12335946500fb07bb3a3434b4a91a891
Natarizki <natakeren890@gmail.com>
2026-05-28 15:05:06 +0700

    Add profiledef.sh and pacman.conf

---
+++ profiledef.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+iso_name="lumialinux"
+iso_label="LUMIALINUX"
+iso_publisher="LumiaLinux"
+iso_application="LumiaLinux Arch-based Distro"
+iso_version="1.0"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
